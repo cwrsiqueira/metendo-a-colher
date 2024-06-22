f289e9a45366e68eb02e1444580189937869f288
cwrsiqueira <cwrsiqueira@msn.com>
2024-06-22 15:46:46 -0300

    Alteração API para o novo GPT-4o, Melhoramento do prompt de requisição, alterações na estilização da página, alteração do link e texto de patrocínio, inclusão de CSS para estilização da página.

--- app/views/home.php
+++ app/views/home.php
@@ -1,24 +1,37 @@
-<div class="container p-3">
-
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
+<nav class="navbar bg-light navbar-expand-lg">
+    <div class="container-fluid">
+        <div class="container">
             <a class="navbar-brand d-flex align-items-center" href="#">
-                <img src="<?= URL; ?>public/assets/img/logo.webp" alt="" width="80" height="80" class="d-inline-block align-text-top">
+                <img src="<?= URL; ?>public/assets/img/logo.webp" alt="Logo" width="80" height="80" class="d-inline-block align-text-top">
                 <div class="d-flex flex-column ms-3">
                     <strong>Metendo a Colher</strong>
-                    <small class="m-0" style="font-size: 0.8rem;">Conselhos Matrimoniais</small>
+                    <small class="text-dark">Conselhos Matrimoniais</small>
                 </div>
             </a>
         </div>
-    </nav>
+    </div>
+</nav>
 
-    <div class="description mt-3" style="font-size: 1rem;">
-        <p>Seja bem vindo!</p>
-        <p>Utilizamos inteligência artifical para dar conselhos matrimoniais e uma sugestão de carta para ser enviada para o seu cônjuge. <br> Melhore seu relacionamento com ajuda de inteligência artifical.</p>
-        <p>Preencha o formulário abaixo e clique Enviar para ver o resultado.</p>
-        <p>Experimente gratuitamente!</p>
-        <p class="fw-bold text-danger" style="font-size: 0.8rem;">Não se preocupe, este site não usa cookies nem armazena nenhum dado ou informação do usuário. <br>
-            Todas as informações coletadas são utilizadas somente para gerar os resultados necessários e descartadas em seguida.</p>
+<div class="container p-3">
+
+    <div class="description mt-3 row">
+        <div class="col-lg fw-semibold">
+            <p>Seja bem-vindo!</p>
+            <p>Utilizamos inteligência artificial para dar conselhos matrimoniais e sugerir uma carta para ser enviada ao seu cônjuge. Melhore seu relacionamento com a ajuda de inteligência artificial.</p>
+            <p>Preencha o formulário abaixo e clique Enviar para ver o resultado.</p>
+            <p>Experimente gratuitamente!</p>
+            <p class="fw-bold text-danger">Não se preocupe, este site não usa cookies nem armazena nenhum dado ou informação do usuário. Todas as informações coletadas são utilizadas somente para gerar os resultados necessários e descartadas em seguida.</p>
+        </div>
+        <div class="col-lg text-center">
+            <div class="text-center p-4 mt-4 fw-semibold">Considere ser um patrocinador com qualquer valor e ajude a manter o site sempre GRATUITO!</div>
+
+            <button class="border border-secondary py-2 px-4 rounded bg-dark text-white fs-6 fw-semibold">
+                <a href="https://www.mercadopago.com.br/subscriptions/checkout?preapproval_plan_id=2c9380848eed4752018eedb8ca7f0039" target="_blank" class='d-flex align-items-center text-white text-decoration-none'>
+                    <i class="fa-solid fa-heart fa-beat text-danger"></i>
+                    &nbsp; Seja um Patrocinador
+                </a>
+            </button>
+        </div>
     </div>
 
     <hr>
@@ -26,7 +39,7 @@
     <?php if (empty($success)) : ?>
 
         <div class="row visually-hidden" id="spinner">
-            <div class="col-sm text-center">
+            <div class="col-lg text-center">
                 <div class="spinner-border" role="status">
                     <span class="visually-hidden">Loading...</span>
                 </div>
@@ -35,21 +48,21 @@
 
         <form method="post" class="mt-3 needs-validation" id="form" novalidate>
             <div class="row">
-                <div class="col-sm form-group">
+                <div class="col-lg form-group">
                     <label for="yourname" class="form-label">1. Seu nome:</label>
                     <input type="text" class="form-control" name="yourname" required>
                     <div class="invalid-feedback">
                         Campo Obrigatório.
                     </div>
                 </div>
-                <div class="col-sm-2 form-group">
+                <div class="col-lg-2 form-group">
                     <label for="yourage" class="form-label">2. Sua idade:</label>
                     <input type="number" class="form-control" name="yourage" required>
                     <div class="invalid-feedback">
                         Campo Obrigatório.
                     </div>
                 </div>
-                <div class="col-sm-4 form-group">
+                <div class="col-lg-4 form-group">
                     <label for="yourjob" class="form-label">3. Seu trabalho:</label>
                     <input type="text" class="form-control" name="yourjob" required>
                     <div class="invalid-feedback">
@@ -58,21 +71,21 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-sm form-group">
+                <div class="col-lg form-group">
                     <label for="spousername" class="form-label">4. Nome do cônjuge:</label>
                     <input type="text" class="form-control" name="spousername" required>
                     <div class="invalid-feedback">
                         Campo Obrigatório.
                     </div>
                 </div>
-                <div class="col-sm-2 form-group">
+                <div class="col-lg-2 form-group">
                     <label for="spouserage" class="form-label">5. Idade do cônjuge:</label>
                     <input type="number" class="form-control" name="spouserage" required>
                     <div class="invalid-feedback">
                         Campo Obrigatório.
                     </div>
                 </div>
-                <div class="col-sm-4 form-group">
+                <div class="col-lg-4 form-group">
                     <label for="spouserjob" class="form-label">6. Trabalho do cônjuge:</label>
                     <input type="text" class="form-control" name="spouserjob" required>
                     <div class="invalid-feedback">
@@ -81,16 +94,15 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-sm-3 form-group">
+                <div class="col-lg-3 form-group">
                     <label for="agemarried" class="form-label">7. Anos de relacionamento: <br> <small>(Em anos)</small></label>
                     <input type="number" name="agemarried" class="form-control" required>
                     <div class="invalid-feedback">
                         Campo Obrigatório.
                     </div>
                 </div>
-                <div class="col-sm form-group">
-                    <label for="focus" class="form-label">8. Pra você, quais são os objetivos do casamento <br> <small>(Até 5
-                            palavras)</small></label>
+                <div class="col-lg form-group">
+                    <label for="focus" class="form-label">8. Pra você, quais são os objetivos do casamento <br> <small>(Até 5 palavras)</small></label>
                     <input type="text" name="focus" class="form-control" required>
                     <div class="invalid-feedback">
                         Campo Obrigatório. Escreva pelo menos 1 palavra.
@@ -98,10 +110,8 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-sm">
-                    <label for="mainproblem" class="form-label">9. Discriminação do problema <br> <small>(Quanto mais
-                            informações e mais precisas forem, serão o diagnóstico
-                            e as sugestões da Inteligência Artifical)</small></label>
+                <div class="col-lg">
+                    <label for="mainproblem" class="form-label">9. Descrição do problema <br> <small>(Quanto mais informações e mais precisas forem, melhores serão o diagnóstico e as sugestões da Inteligência Artificial)</small></label>
                     <textarea name="mainproblem" id="mainproblem" class="form-control" rows="5" required></textarea>
                     <div class="invalid-feedback">
                         Campo Obrigatório.
@@ -109,27 +119,22 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-sm">
-                    <label for="one" class="form-label">10. Se você fosse resumir todos os problemas do seu relacionamento
-                        em até 5 palavras, quais palavras você usaria? <small>(Até 5
-                            palavras)</small></label>
+                <div class="col-lg">
+                    <label for="one" class="form-label">10. Se você fosse resumir todos os problemas do seu relacionamento em até 5 palavras, quais palavras você usaria? <small>(Até 5 palavras)</small></label>
                     <input type="text" name="one" id="one" class="form-control" required>
                     <div class="invalid-feedback">
                         Campo Obrigatório. Escreva pelo menos 1 palavra.
                     </div>
                 </div>
-                <div class="col-sm">
-                    <div class="col-sm form-group">
-                        <label for="obs" class="form-label">11. Alguma outra informação importante?<br> <small>(Se
-                                tiver, altere o texto abaixo)</small></label>
+                <div class="col-lg">
+                    <div class="col-lg form-group">
+                        <label for="obs" class="form-label">11. Alguma outra informação importante?<br> <small>(Se tiver, altere o texto abaixo)</small></label>
                         <textarea name="obs" id="obs" class="form-control" rows="3">Não existem outras informações importantes. Tudo já foi dito anteriormente.</textarea>
                     </div>
                 </div>
             </div>
 
             <button type="submit" class="btn btn-primary mt-3" name="action" value="consultar" id="btn-enviar">Enviar</button>
-
-
         </form>
     <?php else : ?>
 
@@ -138,9 +143,13 @@
             <hr>
             <?= $success ?>
             <hr>
-        <p>Inteligência Artifical pode cometer erros. Considere verificar informações importantes.</p>
+        <p>IMPORTANTE! Inteligência Artificial pode cometer erros. Considere verificar informações importantes.</p>
         <a href="<?php URL; ?>">Voltar</a>
         </p>
 
     <?php endif; ?>
 </div>
+
+<footer class="footer">
+    <p>&copy; 2024 Metendo a Colher - Todos os direitos reservados.</p>
+</footer>
