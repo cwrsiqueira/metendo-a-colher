007da3dd1086004231f620a4271b6fe328168b45
cwrsiqueira <cwrsiqueira@msn.com>
2024-08-19 22:58:27 -0300

    Conclusão do merge das alterações locais e em produção

--- app/views/template.php
+++ app/views/template.php
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= URL; ?>public/assets/css/style.css">
-    <link rel="shortcut icon" href="<?= URL; ?>public/assets/img/favicon.ico" type="image/x-icon">
-    <title><?= ENV['APP_NAME'] ?> | <?= $viewData['pagina'] ?? 'MVC' ?></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <style>
-        label {
-            font-weight: bold;
-        }
-
-        label small {
-            font-weight: lighter;
-        }
-    </style>
-</head>
-
-<body>
-
-    <?php $this->loadViewInTemplate($viewName, $viewData);  ?>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <script src="<?= URL; ?>public/assets/js/script.js"></script>
-</body>
-
-=======
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -82,5 +51,4 @@
     <script src="<?= URL; ?>public/assets/js/script.js"></script>
 </body>
 
->>>>>>> 2b94bd294afc749fff2754a91730e761b28b9daa
 </html>
