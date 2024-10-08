93e0dfbe543596f96a55eb931244d8008d2a1309
Carlos Wagner <cwrsiqueira@msn.com>
2024-10-08 01:03:50 -0300

    Inserido Google Adsense code snippet

--- app/views/template.php
+++ app/views/template.php
@@ -10,8 +10,7 @@
     <meta name="description" content="<?= ENV['APP_DESCRIPTION'] ?>">
     <meta name="robots" content="index, follow">
     <meta name="author" content="Carlos Wagner webdev" />
-    <meta name="keywords"
-        content="relacionamento, casamento, matrimonio, conjugal, conselhos matrimoniais, conselhos conjugais, conselhos amorosos, conselhos para o relacionamento, inteligencia artifical, IA, metendo a colher, carta de amor, amor, romance, vida a dois, vida conjugal, conselho, vida, mantrimonial, relationship, marriage, conjugal, marriage advice, marital advice, love advice, relationship advice, artificial intelligence, AI, spooning, love letter, love, romance, life as a couple, marital life, advice, life, matrimonial" />
+    <meta name="keywords" content="relacionamento, casamento, matrimonio, conjugal, conselhos matrimoniais, conselhos conjugais, conselhos amorosos, conselhos para o relacionamento, inteligencia artifical, IA, metendo a colher, carta de amor, amor, romance, vida a dois, vida conjugal, conselho, vida, mantrimonial, relationship, marriage, conjugal, marriage advice, marital advice, love advice, relationship advice, artificial intelligence, AI, spooning, love letter, love, romance, life as a couple, marital life, advice, life, matrimonial" />
     <meta name="og:title" property="og:title" content="<?= ENV['APP_NAME'] ?> | <?= $viewData['pagina'] ?? 'MVC' ?>">
     <meta property="og:description" content="<?= ENV['APP_DESCRIPTION'] ?>" />
     <meta property="og:url" content="https://metendoacolher.com/" />
@@ -26,27 +25,25 @@
     <link rel="stylesheet" href="<?= URL; ?>public/assets/css/style.css">
     <link rel="shortcut icon" href="<?= URL; ?>public/assets/img/favicon.ico" type="image/x-icon">
     <title><?= ENV['APP_NAME'] ?> | <?= $viewData['pagina'] ?? 'MVC' ?></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
-        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <style>
-    label {
-        font-weight: bold;
-    }
+        label {
+            font-weight: bold;
+        }
 
-    label small {
-        font-weight: lighter;
-    }
+        label small {
+            font-weight: lighter;
+        }
     </style>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5865817649832793" crossorigin="anonymous"></script>
+    <meta name="google-adsense-account" content="ca-pub-5865817649832793">
 </head>
 
 <body>
 
     <?php $this->loadViewInTemplate($viewName, $viewData);  ?>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
     <script src="<?= URL; ?>public/assets/js/script.js"></script>
 </body>
